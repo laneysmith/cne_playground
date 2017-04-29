3bc8d7984c5d999dc7da9e943382e050cb580665
Laney Smith <lsmith.unc@gmail.com>
2017-04-29 16:25:27 -0600

    complete unit 6 exercise 2 - add a database to spring

---
+++ src/main/java/com/example/LessonRepository.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import com.example.models.Lesson;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LessonRepository extends CrudRepository<Lesson, Long> {
+
+
+}
