bc3ce747e436da8e5174f833dbc324ef75128b2a
Laney Smith <lsmith.unc@gmail.com>
2017-04-30 08:52:41 -0600

    complete unit 5 exercise 4 - SpringBootTest

--- src/test/java/com/example/LessonsControllerTest.java
+++ src/test/java/com/example/LessonsControllerTest.java
@@ -9,12 +9,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
 import java.util.Random;
@@ -23,14 +26,13 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(LessonsController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class LessonsControllerTest {
 
     @Autowired
@@ -60,6 +62,8 @@ public class LessonsControllerTest {
     }
 
     @Test
+    @Transactional
+    @Rollback
     public void getAllLessons() throws Exception {
         when(this.lessonRepository.findAll()).thenReturn(Collections.singletonList(lesson));
 
@@ -73,6 +77,8 @@ public class LessonsControllerTest {
     }
 
     @Test
+    @Transactional
+    @Rollback
     public void getLessonById() throws Exception {
         when(this.lessonRepository.findOne(id)).thenReturn(lesson);
 
@@ -87,6 +93,8 @@ public class LessonsControllerTest {
     }
 
     @Test
+    @Transactional
+    @Rollback
     public void postNewLesson() throws Exception {
         MockHttpServletRequestBuilder request = post("/lessons")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -98,6 +106,8 @@ public class LessonsControllerTest {
     }
 
     @Test
+    @Transactional
+    @Rollback
     public void deleteLessonById() throws Exception {
         MockHttpServletRequestBuilder postRequest = post("/lessons")
                 .contentType(MediaType.APPLICATION_JSON)
